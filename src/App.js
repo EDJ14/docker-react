5f16040848db188cd0eec72b9b1a3883800dbf6b
Eddie Jacobs <edjacobs@ucdavis.edu>
2019-06-05 13:58:03 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn karate
         </a>
       </header>
     </div>
